b38350c41364f555725543da3c3f818982a16018
jeffstolzdesign <jeffstolzdesign@gmail.com>
2015-07-31 04:21:39 +0000

    edit features secured

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,32 +1,45 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 5)
   end
 
+
   def show
   	@place = Place.find(params[:id])
   end
 
+
   def new
   	@place = Place.new
   end
+
 
   def create
     current_user.places.create(place_params)
     redirect_to root_path
   end
 
+
   def edit
     @place = Place.find(params[:id])
+	  if @place.user != current_user
+	    return render :text => 'Not Allowed', :status => :forbidden
+	  end
   end
+
 
   def update
   @place = Place.find(params[:id])
+	  if @place.user != current_user
+	    return render :text => 'Not Allowed', :status => :forbidden
+	  end
+
   @place.update_attributes(place_params)
   redirect_to root_path
   end
+
 
   def destroy
   @place = Place.find(params[:id])
@@ -34,10 +47,13 @@ class PlacesController < ApplicationController
   redirect_to root_path
   end
 
+
   private
+
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
+
 
 end
