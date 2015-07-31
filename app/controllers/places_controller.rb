4e416beba86e61bd1b58eb8ff766ffeb6fd6b1a8
jeffstolzdesign <jeffstolzdesign@gmail.com>
2015-07-31 13:59:42 +0000

    validation added to forms

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -17,8 +17,12 @@ class PlacesController < ApplicationController
 
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+  @place = current_user.places.create(place_params)
+	  if @place.valid?
+	    redirect_to root_path
+	  else
+	    render :new, :status => :unprocessable_entity
+	  end
   end
 
 
@@ -37,7 +41,11 @@ class PlacesController < ApplicationController
 	  end
 
   @place.update_attributes(place_params)
-  redirect_to root_path
+	  if @place.valid?
+	    redirect_to root_path
+	  else
+	    render :edit, :status => :unprocessable_entity
+	  end
   end
 
 
